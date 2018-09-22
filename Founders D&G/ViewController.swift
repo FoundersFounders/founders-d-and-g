15d0b220693c9a48b469cfcfe2a6efd31d15f77d
António Ramadas <antonio_ramadas@hotmail.com>
2018-09-22 13:25:02 +0100

    Initial Commit

---
+++ Founders D&G/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Founders D&G
+//
+//  Created by António Ramadas on 22/09/2018.
+//  Copyright © 2018 António Ramadas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
